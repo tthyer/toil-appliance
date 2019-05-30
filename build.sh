4e4c2f214a39651c0eddcb1e829c71a6e9c79321
T. Thyer <tess.thyer@sagebionetworks.org>
2019-05-30 12:26:01 -0700

    Add latest tag

--- build.sh
+++ build.sh
@@ -6,14 +6,17 @@ QUAY_REPO=quay.io/tessthyer/toil-appliance
 SHA=$(git rev-parse HEAD)
 SHORT_SHA=${SHA::7}
 TAG=$QUAY_REPO:$SHORT_SHA
+LATEST_TAG=$QUAY_REPO:latest
 
 echo "Building $TAG."
 docker build -t $TAG .
 
 if [[ $? == 0 ]]; then
+  docker tag $TAG $LATEST_TAG
   read -n 1 -p "Push new image to quay.io (y/n)? " answer
   if [[ "$answer" == "y" ]]; then
     echo "Pushing $TAG."
     docker push $TAG
+    docker push $LATEST_TAG
   fi
 fi
